cd39f0315508a2065c0f7db6fe564d2be4941eba
afifdgr <afifdgr3@gmail.com>
2022-12-02 14:36:09 +0700

    # Cast Index Page

--- app/Http/Controllers/CastController.php
+++ app/Http/Controllers/CastController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cast;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
+
 
 class CastController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Casts/Index');
+        return Inertia::render('Casts/Index', [
+            'casts' => Cast::paginate(5),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 }
